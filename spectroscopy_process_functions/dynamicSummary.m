69e4dd6480c4b001981f6495775d18531d5520c0
Seth Lee <seth@MacBookPro.lan>
2025-03-26 02:18:21 -0400

    Removed sine output in report/CSV and sine fitting calculation steps from report-creating functions.

--- spectroscopy_process_functions/dynamicSummary.m
+++ spectroscopy_process_functions/dynamicSummary.m
@@ -1,5 +1,5 @@
-function [amp_all, nmrFit, nmrFit_ppm, dyn, detrend_sine, fitted_sine, detrend_peaks, fitted_peaks] = ...
-    dynamicSummary(raw_path, dyn_path, BHs, save_fig_flag, rbc_axis_lim, varargin)
+function [amp_all, nmrFit, nmrFit_ppm, dyn, detrend_peaks, fitted_peaks] = ...
+    dynamicSummary(raw_path, dyn_path, BHs, save_fig_flag, rbc_axis_lim)
 % dynamicSummary: produce detrended oscillations and oscillation fits. 
 %       detects and determines if dyn is Voigt or Lorentzian
 % Inputs:
@@ -12,19 +12,13 @@ function [amp_all, nmrFit, nmrFit_ppm, dyn, detrend_sine, fitted_sine, detrend_p
 %       same folder as dyn_path.
 %   rbc_axis_lim: Sets axis limits on rbc oscillation plots. If 0,
 %       automatic limits are used.
-%   Optional: 
-%   dynamicSummary(...,'oscType',method)
-%       available methods are:
-%           'sine' - sine wave identification of amplitude
-%           'peaks' - peak finding identification of amplitude
-%           'peaks_lu' - peak finding identification of amplitude guided by sine fit
 % Outputs:
 %   amp_all: structure with fit amplitudes, heartrate, and dynamic SNR
 %   nmrFit: spectroscopy fit object (Hz)
 %   nmrFit_ppm: spectroscopy fit object (ppm)
 %   dyn: dynamic spectroscopy matlab structure
-%   detrend_sine/peaks: structure containing detrended oscillations
-%   fitted_sine/peaks: structure containing fits to detrended oscillations
+%   detrend_peaks: structure containing detrended oscillations
+%   fitted_peaks: structure containing fits to detrended oscillations
 
 % Example inputs
 % raw_path = '~/Desktop/subject/subject_id/meas_MID00026_FID69955_Xe_fid_DynamicSpec_high_flip.dat';
@@ -38,14 +32,6 @@ function [amp_all, nmrFit, nmrFit_ppm, dyn, detrend_sine, fitted_sine, detrend_p
 % 
 % figname = '';
 % fig_path = fileparts(dyn_path);
-%% This section of the code is not needed as we want to see both "sine" and
-% "peaks" fitting
-
-% p = inputParser;
-% addParameter(p,'suppressFigs',false, @islogical);
-% addParameter(p,'oscType','sine',...
-%    @(x) any(validatestring(x,{'sine','peaks'})));
-% parse(p,varargin{:});
 
 %%
 if isempty(raw_path)
@@ -79,13 +65,6 @@ end
 
 %% Calculate Oscillations Amplitudes and Static Parameters
 
-b = highpassfilter(length(dyn.area(:,1)));
-[amp_sine, detrend_sine, fitted_sine] = calculateOscillationAmps(dyn, BHs, b,'sine','oscType','sine');
-if ~isfield(dyn,'snrs')
-    dyn.snrs = zeros(1,length(dyn.t));
-end 
-amp_sine.snr = mean(dyn.snrs(BHstart:BHend));
-
 % amplitude for the peaks fit
 b = highpassfilter(length(dyn.area(:,1)));
 [amp_peaks, detrend_peaks, fitted_peaks] = calculateOscillationAmps(dyn,BHs,b, 'peaks', 'oscType', 'peaks_lu');
@@ -95,7 +74,6 @@ if ~isfield(dyn,'snrs')
 end 
 amp_peaks.snr = mean(dyn.snrs(BHstart:BHend));
 
-amp_all.sine = amp_sine;
 amp_all.peaks = amp_peaks;
 
 % % Display oscillation results
@@ -114,26 +92,8 @@ amp_all.peaks = amp_peaks;
 
 %% Plots
 
-%%%%%%%%%%%%%%%%%%%%%% Sine Fit figures %%%%%%%%%%%%%%%%%%%%%%%%
-% First, plotting dynamics, oscillations, dynamic SNR, static spectroscopy 
-% for the sine fit, sine is only for oscillations but keeping all the
-% figures for sine fit to prepare the slides seamlessly
-
-figName = 'sine';
-dynSave_path = [dyn_folder,'/dyn',fitType,figName,'.tif'];
-plotDynamics(dyn, BHs, save_fig_flag, dynSave_path);
-
-oscSave_path = [dyn_folder,'/dyn',fitType,'_oscs_new',figName,'.tif'];
-plotOscillations(dyn, BHs, detrend_sine, fitted_sine, rbc_axis_lim, save_fig_flag, oscSave_path)
-
-snrSave_path = [dyn_folder,'/dyn',fitType,'_error',figName,'.tif'];
-plotDynSNR(dyn, BHs, save_fig_flag, snrSave_path)
-
-staticSave_path = [dyn_folder,'/static',fitType,figName,'.tif'];
-plotStaticSpectroscopy(raw_path, nmrFit, save_fig_flag, staticSave_path)
-close all
 %%%%%%%%%%%%%%%%%%%%%% Peaks Fit figures %%%%%%%%%%%%%%%%%%%%%%%%
-% Second, plotting dynamics, oscillations, dynamic SNR, static spectroscopy 
+% Plotting dynamics, oscillations, dynamic SNR, static spectroscopy 
 % for the peaks fit, peaks is only for oscillations but keeping all the
 % figures for peaks fit to prepare the slides seamlessly
 
